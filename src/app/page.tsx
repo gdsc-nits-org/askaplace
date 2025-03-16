4dd66d6c7795a01eca25c1195ed8d63aba846a26
coderSubhadeepdeb <sbhdpdeb@gmail.com>
2025-03-16 21:36:34 +0530

    about section not fully done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import Hero from "../components/Hero";
+import About from "../components/About";
 export default function HomePage() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-hidden flex flex-col">
     <Hero />
+    <About />
     </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 import Image from "next/image";
 function Hero() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-blue-500 bg-[url(https://s3-alpha-sig.figma.com/img/43ac/2e20/d0aab18d1c1468eb9476074402cd5837?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DmFvn5TSfr1YG9XjCEn7lGSOI4xx8YEYQisSCJqJ-NZTYCm7GhNvgnF~Du0Hs1fN-e4x5KYPFgg6KJ9nDwvNSYaWZE7tDn0wZeBr7HmZC6A86JjsBgxgoAwDxSFldJc9llDr07weAQDY12hjVnw~tPeGiFBag~XgMZoruTQlgAUNBgEZPPvqCAmuLN~KReCGmC-4tOQlD0vWKVpWGPS0qSm8cMvHMOKPj2-QMpwhcoX68ECJx1XBN9WEo~XEUkt6dqYsta9Z-oGt4hQGqAOwo9Eo4wdZP4Ue6tzS~2v1Cukj2qhWTBsSeksKps3y9Kqxk~qkr5Ne7BQWV4ZzhWLvSg__)] bg-cover bg-center inset-0 overflow-hidden">
-     
+    <div className="relative flex justify-center items-center w-screen h-screen bg-blue-500 bg-[url(https://s3-alpha-sig.figma.com/img/43ac/2e20/d0aab18d1c1468eb9476074402cd5837?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DmFvn5TSfr1YG9XjCEn7lGSOI4xx8YEYQisSCJqJ-NZTYCm7GhNvgnF~Du0Hs1fN-e4x5KYPFgg6KJ9nDwvNSYaWZE7tDn0wZeBr7HmZC6A86JjsBgxgoAwDxSFldJc9llDr07weAQDY12hjVnw~tPeGiFBag~XgMZoruTQlgAUNBgEZPPvqCAmuLN~KReCGmC-4tOQlD0vWKVpWGPS0qSm8cMvHMOKPj2-QMpwhcoX68ECJx1XBN9WEo~XEUkt6dqYsta9Z-oGt4hQGqAOwo9Eo4wdZP4Ue6tzS~2v1Cukj2qhWTBsSeksKps3y9Kqxk~qkr5Ne7BQWV4ZzhWLvSg__)] bg-cover bg-center inset-0 overflow-hidden">
+       <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-b from-transparent to-gray-600"></div>
       <div className="z-10 flex flex-col items-start justify-start h-auto w-screen text-white pl-8 sm:pl-12 md:pl-20 lg:pl-40 gap-2">
           <span className=" text-4xl lg:text-6xl sm:text-5xl md:text-5xl font-bold text-left">
              ENJOY
