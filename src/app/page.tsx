bcd26c51d9709516d12175ad64be7641117abc1a
GulistaKhatun06 <gulista_ug_23@civil.nits.ac.in>
2025-03-22 11:05:42 +0530

    route added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 import Link from "next/link";
-import Hero from "~/components/Hero";
+
 
 export default function HomePage() {
   return (
-    
-<div>
-<Hero/>
-</div>
+   <div>
+   
+   </div>
 
   )
 }
