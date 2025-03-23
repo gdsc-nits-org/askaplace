99e5f3f26a56530010b82e6d4b4dc9cdc982a8b2
kaushik9859 <bhowmickkaushik1@gmail.com>
2025-03-23 11:09:02 +0530

    Revert "Add descripton page"
    This reverts commit e3e5b5d93b5e39d10696fdd6ba2c21bc25bab703.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,37 @@
-import Navbar from "./components/navbar";
-import ViewMoreButton from "./components/viewMorebutton"; // Import the client component
-import Description from "./components/description";
-import Card from "./components/card";
-import Map from "./components/map";
-import BudgetAnalysis from "./components/budgetAnalysis";
-import dots from "../public/assets/dots.jpg";
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div >
-      <Navbar />
-      <h1 className="text-6xl p-0 font-bold m-40 my-5 sm-m-20">Haflong</h1>
-      <Image src={dots} alt="dots" className="absolute top-20 right-0 -z-10 h-[400px] w-[600px]"/>
-      {/*  the client component is embedded here */}
-      <ViewMoreButton />
-      <Description/>
-      <h1 className="text-5xl p-0 font-medium m-40 my-5 text-center">Top places to visit in Haflong</h1>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Map/>
-      <h1 className="text-5xl p-0 font-medium m-40 my-5 text-center">Food and logding in Haflong</h1>
-      <Card/>
-      <Card/>
-      <Card/>
-      <BudgetAnalysis/>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        </h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="https://create.t3.gg/en/usage/first-steps"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <div className="text-lg">
+              Just the basics - Everything you need to know to set up your
+              database and authentication.
+            </div>
+          </Link>
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+            href="https://create.t3.gg/en/introduction"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Documentation →</h3>
+            <div className="text-lg">
+              Learn more about Create T3 App, the libraries it uses, and how to
+              deploy it.
+            </div>
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
