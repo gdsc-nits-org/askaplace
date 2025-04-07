41d31e44774609c8dbc8712f27dfea278b3540e0
kaushik9859 <bhowmickkaushik1@gmail.com>
2025-04-08 01:09:04 +0530

    making dynamic routes

---
+++ app/feed/[id]/page.jsx
@@ -0,0 +1,41 @@
+import Navbar from "../../components/navbar";
+import ViewMoreButton from "../../components/viewMorebutton";
+import DescriptionContent from "../../components/description";
+import Card from "../../components/card";
+import Map from "../../components/map";
+import BudgetAnalysis from "../../components/budgetAnalysis";
+import dots from "../../../public/assets/dots.jpg";
+import Image from "next/image";
+
+export default function Page({ params }) {
+  const { id } = params; 
+
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-6xl p-0 font-bold m-40 my-5 sm-m-20">{id}</h1>
+      <Image
+        src={dots}
+        alt="dots"
+        className="absolute top-20 right-0 -z-10 h-[400px] w-[600px]"
+      />
+      {/* The client component is embedded here */}
+      <ViewMoreButton />
+      <DescriptionContent />
+      <h1 className="text-5xl p-0 font-medium m-40 my-5 text-center">
+        Top places to visit in {id}
+      </h1>
+      <Card />
+      <Card />
+      <Card />
+      <Map />
+      <h1 className="text-5xl p-0 font-medium m-40 my-5 text-center">
+        Food and lodging in {id}
+      </h1>
+      <Card />
+      <Card />
+      <Card />
+      <BudgetAnalysis />
+    </div>
+  );
+}
