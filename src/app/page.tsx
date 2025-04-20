aa74c276bbf25a5fd0fbdd027d1ac4201b044023
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-04-20 08:21:58 +0530

    resolve conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,7 @@
 
 import Hero from "../components/Hero";
 import About from "../components/About";
-<<<<<<< HEAD
-=======
 import Destination from "../components/Destination";
->>>>>>> e057117d8db8168f4cf8040e265c69073ec02c4c
 
 export default function HomePage() {
   return (
